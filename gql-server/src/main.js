97f1060008fd87643bb3390b9b60b2181e81c6a2
robCrawford <robCrawford@users.noreply.github.com>
2018-12-20 17:53:58 +0000

    Express routes

--- gql-server/src/main.js
+++ gql-server/src/main.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const cors = require("cors");
-const { typeDefs, resolvers, context } = require("./schema");
 const { ApolloServer, gql } = require('apollo-server-express');
+const { typeDefs, resolvers, context } = require("./schema");
+const fs = require('fs');
+const os = require('os');
+
+let version;
+fs.readFile('package.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    version = JSON.parse(data).version;
+});
 
 const PORT = 4000;
-
 const app = express();
-app.use(cors());
-
 const server = new ApolloServer({ typeDefs, resolvers, context });
 server.applyMiddleware({ app });
+
+app.use(cors());
 
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
+
+app.get('/status', (req, res) => {
+    res.status(200).json({
+        timestamp: new global.Date().toISOString(),
+        host: os.hostname(),
+        serviceName: 'graphql',
+        serviceVersion: version
+    });
+});
